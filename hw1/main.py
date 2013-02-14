22ef9ea78b89189a8b2b4eee13c81bbcb085f0d6
Ashok Cutkosky <acutkosky@college.harvard.edu>
2013-02-13 22:19:40 -0500

    try again

--- hw1/main.py
+++ hw1/main.py
@@ -64,6 +64,39 @@ def validateInput(args):
       boostRounds = int(args_map['-b'])
     return [noisyFlag, pruneFlag, valSetSize, maxDepth, boostRounds]
 
+
+def crossvalidation(dataset,numexamples):
+    targetval = dataset.target
+    valcumulativescore = 0.0
+    learncumulativescore = 0.0
+    for i in range(10):
+        learndata =dataset.examples[i*numexamples/10:(i-1)*numexamples/10+numexamples]
+        validationdata = dataset.examples[(i-1)*numexamples/10+numexamples:(i)*numexamples/10+numexamples]
+        old = dataset.examples
+        dataset.examples = learndata
+        train = learn(dataset)
+        valscore = 0.0
+        for example in validationdata:
+            test = classify(train,example)
+            check = example.attrs[targetval]
+            if(test==check):
+                valscore+=1.0
+        valscore = valscore/len(validationdata)
+        learnscore = 0.0
+        for example in learndata:
+            test = classify(train,example)
+            check = example.attrs[targetval]
+            if(test==check):
+                learnscore+=1.0
+        learnscore = learnscore/len(learndata)
+        valcumulativescore +=valscore
+        learncumulativescore +=learnscore
+        dataset.examples = old
+    valcumulativescore /= 10
+    learncumulativescore /= 10
+
+    return valcumulativescore,learncumulativescore
+
 def main():
     arguments = validateInput(sys.argv)
     noisyFlag, pruneFlag, valSetSize, maxDepth, boostRounds = arguments
@@ -81,15 +114,20 @@ def main():
     
     # Copy the dataset so we have two copies of it
     examples = dataset.examples[:]
- 
+    
+
+    numexamples = len(examples)
+
     dataset.examples.extend(examples)
     dataset.max_depth = maxDepth
     if boostRounds != -1:
       dataset.use_boosting = True
       dataset.num_rounds = boostRounds
-
-    # ====================================
-    # WRITE CODE FOR YOUR EXPERIMENTS HERE
+      
+    valscore,learnscore = crossvalidation(dataset,numexamples)
+    print "validation score: ",valscore," learningset score: ",learnscore
+# ====================================
+# WRITE CODE FOR YOUR EXPERIMENTS HERE
     # ====================================
 
 main()
