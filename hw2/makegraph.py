544d3866022f54930450af52a0a86c4be37aa045
Ashok Cutkosky <acutkosky@college.harvard.edu>
2013-02-27 21:49:45 -0500

    fixed some scripts

--- hw2/makegraph.py
+++ hw2/makegraph.py
@@ -7,8 +7,8 @@ data = load(f)
 f.close()
 
 epochs = range(1,len(data))
-training = [x[0] for x in data[1:]]
-validation = [x[1] for x in data[1:]]
+training = [1-x[0] for x in data[1:]]
+validation = [1-x[1] for x in data[1:]]
 pyplot.plot(epochs,training,label = "training data")
 pyplot.plot(epochs,validation,label = "validataion data")
 pyplot.legend()
---
+++ hw2/trials.sh
@@ -0,0 +1,7 @@
+python neural_net_main.py -e 10 -r 1.0 -t hidden -s hiddentest_1
+python neural_net_main.py -e 10 -r 0.1 -t hidden -s hiddentest_01
+python neural_net_main.py -e 10 -r 0.01 -t hidden -s hiddentest_001
+python neural_net_main.py -e 10 -r 0.001 -t hidden -s hiddentest_0001
+python neural_net_main.py -e 10 -r 0.05 -t hidden -s hiddentest_005
+python neural_net_main.py -e 10 -r 0.5 -t hidden -s hiddentest_05
+
