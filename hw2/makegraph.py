feb33a093a254526eb892b5a8a0b9aa9b2b4dff2
Ashok Cutkosky <acutkosky@college.harvard.edu>
2013-02-27 21:33:47 -0500

    added a thing that turns pickle dumps into graphs

---
+++ hw2/makegraph.py
@@ -0,0 +1,19 @@
+from pickle import load
+from sys import argv
+from matplotlib import pyplot
+
+f = open(argv[1],"r")
+data = load(f)
+f.close()
+
+epochs = range(1,len(data))
+training = [x[0] for x in data[1:]]
+validation = [x[1] for x in data[1:]]
+pyplot.plot(epochs,training,label = "training data")
+pyplot.plot(epochs,validation,label = "validataion data")
+pyplot.legend()
+
+if(len(argv)>2):
+    pyplot.title(argv[2])
+
+pyplot.show()
